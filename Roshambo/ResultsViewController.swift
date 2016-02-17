2d52f4873a91c5406f3eca14fa6cd9aa64ad68f4
leanne <leanne@quadzilla.local>
2016-02-17 13:14:23 -0600

    added results view controller file

---
+++ Roshambo/ResultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultsViewController.swift
+//  Roshambo
+//
+//  Created by leanne on 2/17/16.
+//  Copyright © 2016 leanne63. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
